e325e46b2bb97d77e6b8cf6f0c44d482e739ffbf
bamxxls <thaksaporn.xxls@mail.kmutt.ac.th>
2022-09-13 13:56:49 +0700

    done laew

--- minMedMax.js
+++ minMedMax.js
@@ -1,6 +1,11 @@
 const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
-  //code here
+
+    let max = n1>n2 && n1>n3 ? n1 : n2>n3 ? n2 : n3;
+    let min = n3<n2 && n3<n1 ? n3 : n2<n1 ? n2 : n1;
+    let mid = n1>=n2&&n1<=n3 || n1>=n3&&n1<=n2 ? n1 : n2>=n1&&n2<=n3 || n2>=n3&&n2<=n1 ? n2 : n3;
+
+    return { min : min, mid: mid, max : max};
 }
 module.exports = minMedMax
